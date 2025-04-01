936349e918adf12fe13d8a79ca568ca1a1f54561
boris <boris.isac@webtech87.com>
2025-04-01 11:46:25 +0100

    [ADD] employees_app

--- timeManagment/main/urls.py
+++ timeManagment/main/urls.py
@@ -6,7 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', index, name = 'index'),
-    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
+    #path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
 
 ]
