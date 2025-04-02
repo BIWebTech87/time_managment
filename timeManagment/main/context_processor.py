97d30999fc3cfd58e57d4b8e00301a3218b805a0
boris <boris.isac@webtech87.com>
2025-04-02 09:54:52 +0100

    [ADD] list_employeers processor for navbar

---
+++ timeManagment/main/context_processor.py
@@ -0,0 +1,6 @@
+def custom_processor(request):
+    return {
+        'user': request.user if request.user.is_authenticated else None,
+        'is_superuser': request.user.is_superuser if request.user.is_authenticated else False,
+
+    }
--- timeManagment/timeManagment/settings.py
+++ timeManagment/timeManagment/settings.py
@@ -69,6 +69,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                #my custom processors
+                'main.context_processor.custom_processor',
             ],
         },
     },
