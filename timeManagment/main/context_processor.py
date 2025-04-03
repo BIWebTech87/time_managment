8aba445b8882d28c8e790787bc1ebcb0d83c52fb
boris <boris.isac@webtech87.com>
2025-04-03 17:38:14 +0100

    [ADD] frontend react

--- timeManagment/main/context_processor.py
+++
@@ -1,5 +0,0 @@
-def custom_processor(request):
-    return {
-        'user': request.user if request.user.is_authenticated else None,
-        'is_superuser': request.user.is_superuser if request.user.is_authenticated else False,
-    }
