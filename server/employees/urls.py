41d3a8833be05416bc0144e5035de44734cec852
boris <boris.isac@webtech87.com>
2025-04-17 15:27:54 +0100

    [ADD] documentation

--- server/employees/urls.py
+++ server/employees/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from rest_framework import routers
-
 from .views import EmployeeVeiwSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
--- server/employees/views.py
+++ server/employees/views.py
@@ -1,23 +1,24 @@
-from django.core.checks import messages
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
-from django.views import View
+
 from .models import Employee
-from .forms import EmployeeDataForm, NewEmployeeForm, DeleteEmployeeForm, EmployeeLoginForm
+from .forms import EmployeeLoginForm
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from rest_framework import viewsets
 from .serializers import EmployeeSerializer
 from django.contrib.auth.views import LoginView
 from rest_framework.permissions import IsAuthenticated
 from .perrmitions import IsAuthenticatedAndSuperuser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 class EmployeeVeiwSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     http_method_names = ['get','post', 'put', 'patch']
     permission_classes = [IsAuthenticatedAndSuperuser]
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 class EmployeeLoginView(LoginView):
     template_name = 'login.html'
     form_class = EmployeeLoginForm
